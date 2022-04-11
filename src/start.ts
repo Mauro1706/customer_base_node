987aa82e071a36d9cb19e820f8b452b7d94d3d59
Mauro Costa <mauro@cuponeria.com.br>
2022-04-11 10:58:16 -0300

    Implementando docker

--- src/start.ts
+++ src/start.ts
@@ -6,20 +6,10 @@ import Database from './database/db';
 import ClientController from "./controller/clientController";
 import ContractStatusController from "./controller/contractStatusController";
 
-import swaggerUi = require('swagger-ui-express');
-import fs = require('fs');
-
 class App {
     public app: express.Application;
     private _db: Database;
     private bodyParser;
-
-    /* Swagger files start */
-    private swaggerFile: any = (process.cwd()+"/swagger/swagger.json");
-    private swaggerData: any = fs.readFileSync(this.swaggerFile, 'utf8');
-    private customCss: any = fs.readFileSync((process.cwd()+"/swagger/swagger.css"), 'utf8');
-    private swaggerDocument = JSON.parse(this.swaggerData);
-    /* Swagger files end */
 
     constructor() {
         this.app = express();   
@@ -72,9 +62,9 @@ class App {
         this.app.route('/api/v1/contract/status/:id').put(ContractStatusController.update)
         this.app.route('/api/v1/contract/status/:id').delete(ContractStatusController.delete)
 
-        // swagger docs
-        this.app.use('/api/docs', swaggerUi.serve,
-            swaggerUi.setup(this.swaggerDocument, null, null, this.customCss));
+        // // swagger docs
+        // this.app.use('/api/docs', swaggerUi.serve,
+        //     swaggerUi.setup(this.swaggerDocument, null, null, this.customCss));
     }
 }
 
