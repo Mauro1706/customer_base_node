e2769f7aae1e2e99302705e17c4f3fe9e5640b28
Mauro Costa <mauro@cuponeria.com.br>
2022-04-11 11:11:32 -0300

    Correções docker

--- src/database/db.ts
+++ src/database/db.ts
@@ -1,7 +1,7 @@
 import * as mongoose from "mongoose";
 
 class Database {
-  private DB_URL = "mongodb://link-db/dbClient";
+  private DB_URL = "mongodb://localhost:27017/dbClient";
 
   createConnection() {
     mongoose.connect(this.DB_URL)
